ac91316ff05feb852ab06d68a981e58aedc57dea
Siddarth <pullakhandam.siddartha@gmail.com>
2023-12-06 20:19:01 -0600

    commit

--- flask_app.py
+++ flask_app.py
@@ -67,13 +67,84 @@
 
 # if __name__ == '__main__':
 #     app.run(port=5002)
-from flask import Flask, request, render_template
+# from flask import Flask, request, render_template
+# from langchain.chains import ConversationalRetrievalChain
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.chat_models import ChatOpenAI
+# from langchain.memory import ConversationBufferWindowMemory
+# from langchain.vectorstores.deeplake import DeepLake
+# import os
+# from dotenv import load_dotenv
+# import re
+# # Load environment variables from the .env file
+# load_dotenv()
+
+# # Access the OpenAI key
+# openai_key = os.getenv("OPENAI_API_KEY")
+# activeloop_key = os.getenv("ACTIVELOOP_TOKEN")
+
+# app = Flask(__name__)
+
+# # Load the existing DeepLake dataset
+# dataset_path = f"hub://siddartha10/manufacturing_CSI"
+# db = DeepLake(
+#     dataset_path=dataset_path,
+#     embedding=OpenAIEmbeddings(),
+#     runtime={"tensor_db": True},
+#     token=activeloop_key,
+#     read_only=True,
+# )
+
+# # Configure the retriever with deep_memory=True
+# retriever = db.as_retriever()
+# retriever.search_kwargs["deep_memory"] = True
+# retriever.search_kwargs["k"] = 10
+
+# # Initialize LangChain pipeline
+# llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
+# memory = ConversationBufferWindowMemory(
+#         memory_key='chat_history', return_messages=True)
+# conversation_chain = ConversationalRetrievalChain.from_llm(
+#         llm=llm,
+#         chain_type="stuff",
+#         retriever=retriever,
+#         memory=memory
+#     )
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/generate_answer', methods=['POST'])
+# def generate_answer():
+#     try:
+#         data = request.get_json()
+#         prompt = data['prompt']        
+
+#         # Run LangChain pipeline to generate the answer
+#         prompt_inbuilt = """You are a world class expert for generating questions based on provided context. 
+#         and you will answer question only based on the given context."""
+#         result = conversation_chain.run(prompt_inbuilt+prompt)
+
+#         # Extract the answer from the LangChain output
+#         answer = result
+#         #print(answer)
+#         #print(type(answer))
+#         return {"answer": answer}
+
+#     except Exception as e:
+#         return {"error": str(e)}
+
+# if __name__ == '__main__':
+#     app.run(port=5002)
+from flask import Flask, request, jsonify, render_template
 from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.vectorstores.deeplake import DeepLake
 import os
+import json
 from dotenv import load_dotenv
 
 # Load environment variables from the .env file
@@ -85,13 +156,14 @@ activeloop_key = os.getenv("ACTIVELOOP_TOKEN")
 
 app = Flask(__name__)
 
-# Load the existing DeepLake dataset
+# # Load the existing DeepLake dataset
 dataset_path = f"hub://siddartha10/manufacturing_CSI"
 db = DeepLake(
-    dataset_path=dataset_path,
+    dataset_path=dataset_path,  # org_id stands for your username or organization from activeloop
     embedding=OpenAIEmbeddings(),
     runtime={"tensor_db": True},
     token=activeloop_key,
+    # overwrite=True, # user overwrite flag if you want to overwrite the full dataset
     read_only=True,
 )
 
@@ -120,20 +192,27 @@ def generate_answer():
     try:
         data = request.get_json()
         prompt = data['prompt']
+        print(prompt)
 
         # Run LangChain pipeline to generate the answer
-        prompt_inbuilt = """You are a world class expert for generating questions based on provided context. 
-        and you will answer question only based on the given context."""
-        result = conversation_chain.run(prompt_inbuilt+prompt)
+        result = conversation_chain.run(prompt)
 
         # Extract the answer from the LangChain output
         answer = result
-        #print(answer)
-        #print(type(answer))
-        return {"answer": answer}
+        
+        return app.response_class(
+            response=json.dumps({"message": answer, "status": "success"}),
+            status=200,
+            mimetype='application/json'
+        )
 
     except Exception as e:
-        return {"error": str(e)}
+        print(e)
+        return app.response_class(
+            response=json.dumps({"message": "Error While generating answers, Please try again", "status": "error"}),
+            status=500,
+            mimetype='application/json'
+        )
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(port=5002, debug=True)
